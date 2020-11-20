18a57eba920d2ce727b14124f9b8f8534b514cbd
ArmanAttaran <armanattaran@gmail.com>
2020-11-19 21:26:21 -0500

    test

---
+++ code/Publishing_script.R
@@ -0,0 +1,5 @@
+wflow_publish(c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd","analysis/suppfiles.Rmd"),
+              "Publish the initial files for myproject")
+wflow_open("analysis/suppfiles.Rmd")
+wflow_build()
+wflow_open("analysis/index.Rmd")
