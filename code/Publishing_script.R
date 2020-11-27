c4771ad5195f1547df4d0d87c992c800cb01054e
ArmanAttaran <armanattaran@gmail.com>
2020-11-27 02:39:33 -0500

    added some seph tables

--- code/Publishing_script.R
+++ code/Publishing_script.R
@@ -4,7 +4,7 @@ wflow_publish(c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rm
               "Publish the initial files for myproject")
 wflow_open("analysis/suppfiles.Rmd")
 wflow_build()
-wflow_open("analysis/index.Rmd")
+wflow_open("analysis/Monthly_provincial_nowcast.Rmd")
 
 wflow_status(
 )
