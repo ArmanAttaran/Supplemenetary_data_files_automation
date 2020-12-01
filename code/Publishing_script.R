6478cc4ffa5d32f262c2c0adcaad6c4bf7d755a3
ArmanAttaran <armanattaran@gmail.com>
2020-11-30 23:39:17 -0500

    some excersices for graphing for inspiration

--- code/Publishing_script.R
+++ code/Publishing_script.R
@@ -2,7 +2,7 @@ library(workflowr
         )
 wflow_publish(c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd","analysis/supp_files.Rmd","analysis/prov_analysis.rmd"),
               "Publish the initial files for myproject")
-wflow_open("analysis/suppfiles.Rmd")
+wflow_open("analysis/excercise.Rmd")
 wflow_build()
 wflow_open("analysis/Monthly_provincial_nowcast.Rmd")
 
