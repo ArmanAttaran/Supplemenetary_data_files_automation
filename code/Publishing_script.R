6ab2d839738711150f5f306478ef32cc23355eac
arman <armanattaran@gmail.com>
2021-03-31 23:25:43 -0400

    testing new stuff

--- code/Publishing_script.R
+++ code/Publishing_script.R
@@ -1,10 +1,16 @@
 library(workflowr
         )
-wflow_publish(c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd","analysis/supp_files.Rmd","analysis/prov_analysis.rmd"),
+wflow_publish(c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd","analysis/supp_files.Rmd","analysis/prov_analysis.rmd","analysis/supp_files.Rmd","analysis/google_mobility_data.Rmd"),
               "Publish the initial files for myproject")
-wflow_open("analysis/excercise.Rmd")
+wflow_open("analysis/google_mobility_data.Rmd")
 wflow_build()
-wflow_open("analysis/Monthly_provincial_nowcast.Rmd")
+wflow_open("analysis//graphing_heierachies.rmd")
 
 wflow_status(
 )
+
+wflow_publish(c("analysis/Provincial_view.rmd","analysis/index.Rmd","analysis/google_mobility_data.Rmd"))
+
+
+wflow_publish(c("analysis/index.Rmd","analysis/google_mobility_data.Rmd"))
+
